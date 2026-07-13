e845ce44bd88de0a79f791a9ea36ab3f6c241052
Geert Claes <UI517987@rwe.com>
2026-07-13 13:15:29 +0200

    Remove admin token requirement

--- app/admin/components/AdminShell.jsx
+++ app/admin/components/AdminShell.jsx
@@ -1,15 +1,12 @@
 "use client";
 
 import AdminNav from "./AdminNav";
-import AdminTokenGate from "./AdminTokenGate";
 
 export default function AdminShell({ children }) {
   return (
-    <AdminTokenGate>
-      <div className="admin-layout">
-        <AdminNav />
-        <div className="admin-layout-main">{children}</div>
-      </div>
-    </AdminTokenGate>
+    <div className="admin-layout">
+      <AdminNav />
+      <div className="admin-layout-main">{children}</div>
+    </div>
   );
 }
